071c7b288a88020f4a43bc0bb6c1ea0b70edbbe3
titosilva <jt.mat@hotmail.com>
2020-10-20 16:55:36 -0300

    feature: adding format field to ByteString

--- pkg/bytestring/ByteString.go
+++ pkg/bytestring/ByteString.go
@@ -4,6 +4,9 @@ package bytestring
 // Define um tipo comum a ser consumido
 // por todas as funções de criptografia
 type ByteString struct {
+	// String que poderá ser usada pelos encoders
+	// Como forma de indicar algo sobre o formato
+	format string
 	// Array de bytes para armazenar o valor
 	bytes []byte
 }
