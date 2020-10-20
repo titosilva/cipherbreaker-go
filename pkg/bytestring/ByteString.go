b25866ef0552d61edeb1d24ce9dace5f6f4328a8
titosilva <jt.mat@hotmail.com>
2020-10-20 16:54:14 -0300

    feature: adding basic ByteString struct

---
+++ pkg/bytestring/ByteString.go
@@ -0,0 +1,9 @@
+package bytestring
+
+// ByteString struct
+// Define um tipo comum a ser consumido
+// por todas as funções de criptografia
+type ByteString struct {
+	// Array de bytes para armazenar o valor
+	bytes []byte
+}
