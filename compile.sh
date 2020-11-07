ec9247d4498047ec5824973e29d60459c0e8f8a3
titosilva <jt.mat@hotmail.com>
2020-11-07 20:11:31 -0300

    refactor: removing bytestring package code

--- compile.sh
+++ compile.sh
@@ -2,18 +2,3 @@
 
 SRC="github.com/titosilva/cipherbreaker-go/"
 
-if [[ "$1" == "bytestring" ]]
-then
-    echo "Compiling $1..."
-    # Compile bytestring package
-    go build "$SRC/pkg/bytestring"
-    echo "Done"
-fi
-
-if [[ "$1" == "coder" ]]
-then
-    echo "Compiling $1..."
-    # Compile bytestring package
-    go build "$SRC/pkg/bytestring/coder"
-    echo "Done"
-fi
