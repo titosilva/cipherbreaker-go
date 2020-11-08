2ec9fdbd725268cbc5994ed865a76af484460744
titosilva <jt.mat@hotmail.com>
2020-11-07 23:56:13 -0300

    style: chaging names in textanalysis package

--- pkg/textanalysis/Analyzer.go
+++
@@ -1,7 +0,0 @@
-package textanalysis
-
-// Analyzer interface
-// Receives a text, and returns some information about that text
-type Analyzer interface {
-	Analyze(args ...interface{}) interface{}
-}
