ce22c147b0ebc7061498ef18972a4f7878766a91
titosilva <jt.mat@hotmail.com>
2020-11-08 18:04:37 -0300

    feature: changing return of WordMatcher from error to index

--- pkg/textanalysis/algorithms/wordmatcher/WordMatcher.go
+++ pkg/textanalysis/algorithms/wordmatcher/WordMatcher.go
@@ -12,9 +12,9 @@ type WordMatcher struct {
 
 // Analyse method of WordMatcher
 // Goes through all the text, trying to match word
-// when complete, returns an error if any problem is
-// or returns a bool indicating if there was a match
-func (wordMatcher WordMatcher) Analyse(originalText string) (bool, error) {
+// when complete returns a bool indicating if there was a match
+// and returns where did the match occurr (-1 if no match is found)
+func (wordMatcher WordMatcher) Analyse(originalText string) (bool, int) {
 	// Initialize counter as 0
 	counter := 0
 
@@ -24,12 +24,12 @@ func (wordMatcher WordMatcher) Analyse(originalText string) (bool, error) {
 
 	// Iterates over all the text, and for each char, tries to
 	// find a match with the given word (or any other given string)
-	for _, c := range text {
+	for idx, c := range text {
 		// Uses the counter to know where it is in the word being matched
 		if byte(c) == lowerWord[counter] {
 			//  If it reaches the end of the word, return true
 			if counter == len(lowerWord)-1 {
-				return true, nil
+				return true, idx - counter
 			}
 
 			// if not, add 1 to counter
@@ -42,5 +42,5 @@ func (wordMatcher WordMatcher) Analyse(originalText string) (bool, error) {
 
 	// If the execution reaches here, the code above did
 	// not return. So, there was no match
-	return false, nil
+	return false, -1
 }
--- pkg/textanalysis/algorithms/wordmatcher/WordMatcher_test.go
+++ pkg/textanalysis/algorithms/wordmatcher/WordMatcher_test.go
@@ -9,27 +9,31 @@ func TestWordMatcher(t *testing.T) {
 		// String to test
 		caseStr string
 		// Expected response
-		expected bool
+		expectedMatch bool
+		expectedIdx   int
 		// Indicates if the test case is a
 		// success or a fail case
 		success bool
 	}{
 		{
-			caseWord: "abacaxi",
-			caseStr:  "O abadaxi estava muito bom. Ops, quero dizer, abacaxi",
-			expected: true,
-			success:  true,
+			caseWord:      "abacaxi",
+			caseStr:       "O abadaxi estava muito bom. Ops, quero dizer, abacaxi",
+			expectedMatch: true,
+			expectedIdx:   46,
+			success:       true,
 		}, {
-			caseWord: "abacaxi",
-			caseStr:  "O abadaxi estava muito bom.",
-			expected: false,
-			success:  true,
+			caseWord:      "abacaxi",
+			caseStr:       "O abadaxi estava muito bom.",
+			expectedMatch: false,
+			expectedIdx:   -1,
+			success:       true,
 		}, {
 			// Test case sensitibility - must be case insensitive
-			caseWord: "Hund",
-			caseStr:  "Dein hund ist sehr klug",
-			expected: true,
-			success:  true,
+			caseWord:      "Hund",
+			caseStr:       "Dein hund ist sehr klug",
+			expectedMatch: true,
+			expectedIdx:   5,
+			success:       true,
 		},
 	}
 
@@ -37,14 +41,14 @@ func TestWordMatcher(t *testing.T) {
 	for _, c := range testcases {
 		if c.success {
 			// Success cases
-			match, err := WordMatcher{Word: c.caseWord}.Analyse(c.caseStr)
+			match, idx := WordMatcher{Word: c.caseWord}.Analyse(c.caseStr)
 
-			if err != nil {
-				t.Errorf("WordMatcher FAILED: %s -> expected: %t; got ERROR: %s", c.caseWord, c.expected, err)
+			if idx != c.expectedIdx {
+				t.Errorf("WordMatcher FAILED: %s -> expected index: %d; got index: %d", c.caseWord, c.expectedIdx, idx)
 			}
 
-			if match != c.expected {
-				t.Errorf("WordMatcher FAILED: %s -> expected: %t; got: %t", c.caseWord, c.expected, match)
+			if match != c.expectedMatch {
+				t.Errorf("WordMatcher FAILED: %s -> expected: %t; got: %t", c.caseWord, c.expectedMatch, match)
 			}
 		} else {
 			// Fail cases
