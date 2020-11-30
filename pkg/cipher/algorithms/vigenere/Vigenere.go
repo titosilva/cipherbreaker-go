27eea340e64bb008c3ccd6458b035730cf372b7a
ronaldcdo <ronaldcesar.eng@gmail.com>
2020-11-30 11:34:20 -0300

    Vigener's Decipher Method  (Test is not working)!

--- pkg/cipher/algorithms/vigenere/Vigenere.go
+++ pkg/cipher/algorithms/vigenere/Vigenere.go
@@ -7,7 +7,7 @@ type Vigenere struct{}
 
 // Vigenere's cipher implementation.
 
-// Cipher method for Vigenere Cipher
+// Cipher method for Vigenere's Cipher
 // Takes arguments as string slices to enforce use of ASCII encoding
 // (golang doesn't have a char[] type, but has byte[] and rune[])
 func (c Vigenere) Cipher(plainTextString string, key string) (string, error) {
@@ -26,6 +26,7 @@ func (c Vigenere) Cipher(plainTextString string, key string) (string, error) {
 		}
 	}
 
+	//Equalling the size of the key with the text, if the key is smaller than the text
 	for i, aux := 0, 0; i < len(plainText); i++ {
 		if len(keyAlpha) < len(plainText) && len(keyAlpha) > 1 && keyAlpha[i] == 0 && plainText[i] != 0 {
 			keyAlpha[i] = keyAlpha[aux]
@@ -37,6 +38,7 @@ func (c Vigenere) Cipher(plainTextString string, key string) (string, error) {
 		}
 	}
 
+	//Encoding the plainText into the cipherText using the keyAlpha
 	for idx := range plainText {
 		if cipherText[idx] >= 'a' && cipherText[idx] <= 'z' {
 			cipherText[idx] = (plainText[idx]+keyAlpha[idx])%26 + 'a'
@@ -49,15 +51,18 @@ func (c Vigenere) Cipher(plainTextString string, key string) (string, error) {
 	return string(cipherText), nil
 }
 
-/*
-// Decipher method
+// Vigenere's Decipher implementation
 
+// Decipher method for Vigenere's Cipher
+// Takes arguments as string slices to enforce use of ASCII encoding
+// (golang doesn't have a char[] type, but has byte[] and rune[])
 func (c Vigenere) Decipher(cipherTextString string, key string) (string, error) {
-	plainText := []byte(cipherTextString)
-	cipherText := make([]byte, len(plainText))
+	cipherText := []byte(cipherTextString)
+	decipherText := make([]byte, len(cipherText))
 	var keyAlpha []byte
 
-	for i := 0; i < len(plainText); i++ {
+	// Standardizing the format of the key
+	for i := range key {
 		if (key[i] >= 'a') && (key[i]) <= 'z' {
 			keyAlpha[i] = key[i] - 'a'
 		} else if (key[i] >= 'A') && (key[i] <= 'Z') {
@@ -67,14 +72,27 @@ func (c Vigenere) Decipher(cipherTextString string, key string) (string, error) 
 		}
 	}
 
-	for idx := range plainText {
-		if cipherText[idx] >= 'a' && cipherText[idx] <= 'z' {
-			cipherText[idx] = (plainText[idx]-keyAlpha[idx]+26)%26 + 'a'
-		} else if cipherText[idx] >= 'A' && cipherText[idx] <= 'Z' {
-			cipherText[idx] = (plainText[idx]-keyAlpha[idx]+26)%26 + 'A'
+	//Equalling the size of the key with the text, if the key is smaller than the text
+	for i, aux := 0, 0; i < len(cipherText); i++ {
+		if len(keyAlpha) < len(cipherText) && len(keyAlpha) > 1 && keyAlpha[i] == 0 && cipherText[i] != 0 {
+			keyAlpha[i] = keyAlpha[aux]
+			aux++
+		} else if len(key) < len(cipherText) {
+			keyAlpha[i] = cipherText[i]
 		} else {
-			cipherText[idx] = keyAlpha[idx]
+			break
 		}
 	}
-	return string(cipherText), nil
-}*/
+
+	//Decoding the plainText into the cipherText using the keyAlpha
+	for idx := range cipherText {
+		if decipherText[idx] >= 'a' && decipherText[idx] <= 'z' {
+			decipherText[idx] = (cipherText[idx]-keyAlpha[idx]+26)%26 + 'a'
+		} else if decipherText[idx] >= 'A' && decipherText[idx] <= 'Z' {
+			decipherText[idx] = (cipherText[idx]-keyAlpha[idx]+26)%26 + 'A'
+		} else {
+			decipherText[idx] = keyAlpha[idx]
+		}
+	}
+	return string(decipherText), nil
+}
--- pkg/cipher/algorithms/vigenere/Vigenere_test.go
+++ pkg/cipher/algorithms/vigenere/Vigenere_test.go
@@ -46,3 +46,45 @@ func TestVigenereCipher(t *testing.T) {
 		}
 	}
 }
+
+func TestVigenereDecipher(t *testing.T) {
+	var vigenere Vigenere
+
+	cases := []struct {
+		caseString string
+		caseKey    string
+		expected   string
+		// Tells if a case is of success or fail
+		success bool
+	}{
+		{
+			caseString: "bacba",
+			caseKey:    "bac",
+			expected:   "abcde",
+			success:    true,
+		},
+	}
+	for _, c := range cases {
+		if c.success {
+			// Success cases
+			t.Logf("Vigenere testing: %s <key: %s> -> %s", c.caseString, c.caseKey, c.expected)
+			result, err := vigenere.Cipher(c.caseString, c.caseKey)
+
+			if err != nil {
+				t.Errorf("Vigenere FAILED: %s <key: %s> -> expected: %s; got ERROR: %s", c.caseString, c.caseKey, c.expected, err)
+			}
+
+			if result != c.expected {
+				t.Errorf("Vigenere FAILED: %s <key: %s> -> expected: %s; got: %s", c.caseString, c.caseKey, c.expected, result)
+			}
+		} else {
+			// Fail cases
+			t.Logf("Vigenere testing: %s <key: %s> -> expected err", c.caseString, c.caseKey)
+			result, err := vigenere.Cipher(c.caseString, c.caseKey)
+
+			if err == nil {
+				t.Errorf("Vigenere FAILED: %s <key: %s> -> expected error, but got: %s", c.caseString, c.caseKey, result)
+			}
+		}
+	}
+}
