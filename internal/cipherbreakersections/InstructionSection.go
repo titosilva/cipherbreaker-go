09f70aba230a3ec6f5a57713456d046a13f47463
ronaldcdo <ronaldcesar.eng@gmail.com>
2020-12-07 20:41:08 -0300

    Some instructions information

--- internal/cipherbreakersections/InstructionSection.go
+++ internal/cipherbreakersections/InstructionSection.go
@@ -18,20 +18,32 @@ func (s InstructionSection) Run() section.Section {
 	defer insView.Kill()
 
 	width, heigth := screen.GetSize()
-	// w := uint(width)
+
 
 	// Scrollable
 	scrollable := renderable.NewScrollableContainer(width, heigth)
 
-	// Welcome
+	// Welcome	
 	welcomeHeader := renderable.NewText("WELCOME TO CIPHERBREAKER!", 0, 0)
 	welcome := renderable.NewText("Hello! Glad you're here. With this software you may use or break some kinds of Cipher. Currently, Caesar and Vigenere cipher are "+
 		"the algorithms supported.", 1, 0)
 
+		And this project has a lot of cool stuff
+		dynamic rendering is veeeeeeeery cool...
+
+	instructionsOptions := renderable.NewText("The Cipher/Decipher option is responsible to open the options to encode or decode using Caesar or Vigenere's Cipher ", 5, 0)
+	caesarOption:= renderable.NewText("Selecting the Caesar option you can choose if you want to get the encoded/decoded text from a file, giving the path and typing the key that you want."+
+	"After, you can select if you want to Decipher or Cipher the text using that key", 6, 0)
+	
 	welcome.Wrapped(width - 2)
+	instructionsOptions.Wrapped(width - 2)
+	caesarOption.Wrapped(width - 2)
 
 	scrollable.InternalContainer.AddItem(&welcomeHeader)
 	scrollable.InternalContainer.AddItem(&welcome)
+	scrollable.InternalContainer.AddItem(&instructionsOptions)
+	scrollable.InternalContainer.AddItem(&caesarOption)
+	// scrollable.InternalContainer.AddItem(&caesarOption2)
 
 	insView.ViewContainer.AddItem(&scrollable)
 
